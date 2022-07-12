2c2945fd2a6bf5a07187a6d3471d0688e5540c20
saratedd <sara.tedesko91@gmail.com>
2022-07-12 11:39:40 +0200

    add background color and imgs

--- app/src/main/java/com/example/shows_saratedd/MainActivity.kt
+++ app/src/main/java/com/example/shows_saratedd/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.shows_saratedd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shows_saratedd.databinding.ActivityLoginBinding
+import com.example.shows_saratedd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
