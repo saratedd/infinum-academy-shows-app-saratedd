c58c7f607b96ef9a7b7f869bab0364d1e63a2c26
saratedd <sara.tedesko91@gmail.com>
2022-07-13 17:08:37 +0200

    add broken welcome screen

--- app/src/main/java/com/example/shows_saratedd/MainActivity.kt
+++ app/src/main/java/com/example/shows_saratedd/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.shows_saratedd
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.shows_saratedd.databinding.ActivityLoginBinding
@@ -13,6 +14,19 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var email = ""
+        if (binding.emailEditTextField.text.toString().contains("@")) {
+            email = binding.emailEditTextField.text.toString().substringBefore("@", "")
+            binding.loginButton.isEnabled = true
+
+            binding.loginButton.setOnClickListener {
+                val intent = Intent(this, WelcomeActivity::class.java)
+                intent.putExtra("email", email)
+                startActivity(intent)
+            }
+        }
+
 
 
     }
