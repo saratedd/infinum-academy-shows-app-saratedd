21875c4a98c5bc5f813d7498af7dbde6ff131874
saratedd <sara.tedesko91@gmail.com>
2022-07-19 01:44:39 +0200

    add showdetailsactivity

--- app/src/main/java/com/example/shows_saratedd/ShowDetailsActivity.kt
+++ app/src/main/java/com/example/shows_saratedd/ShowDetailsActivity.kt
@@ -2,10 +2,34 @@ package com.example.shows_saratedd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shows_saratedd.databinding.ActivityShowDetailsBinding
+import com.example.shows_saratedd.databinding.DialogAddReviewBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class ShowDetailsActivity : AppCompatActivity() {
+    lateinit var binding: ActivityShowDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_show_details)
+
+        binding = ActivityShowDetailsBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.detailsTitle.text = intent.extras?.getString("showName")
+        binding.detailsDesc.text = intent.extras?.getString("showDesc")
+        intent.extras?.getInt("showImage")?.let { binding.detailsImg.setImageResource(it) }
+//        binding.detailsImg.setImageResource(intent.extras?.getInt("showImage"))
+
+        binding.detailsReviewsButton.setOnClickListener {
+            val dialog = BottomSheetDialog(this)
+            val bottomSheetBinding = DialogAddReviewBinding.inflate(layoutInflater)
+            dialog.setContentView(bottomSheetBinding.root)
+
+            bottomSheetBinding.submitButton.setOnClickListener {
+                dialog.dismiss()
+            }
+            dialog.show()
+        }
     }
 }
