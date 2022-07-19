428ef02af8b04a3f82c275d77966797354e63b35
saratedd <sara.tedesko91@gmail.com>
2022-07-19 14:46:11 +0200

    add back button to showdetails

--- app/src/main/java/com/example/shows_saratedd/ShowDetailsActivity.kt
+++ app/src/main/java/com/example/shows_saratedd/ShowDetailsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.shows_saratedd
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.shows_saratedd.databinding.ActivityShowDetailsBinding
@@ -22,6 +23,10 @@ class ShowDetailsActivity : AppCompatActivity() {
         intent.extras?.getInt("showImage")?.let { binding.detailsImg.setImageResource(it) }
 //        binding.detailsImg.setImageResource(intent.extras?.getInt("showImage"))
 
+        binding.detailsBackButton.setOnClickListener {
+            val intentBack = Intent(this, ShowsActivity::class.java)
+            startActivity(intentBack)
+        }
         binding.detailsReviewsButton.setOnClickListener {
             val dialog = BottomSheetDialog(this)
             val bottomSheetBinding = DialogAddReviewBinding.inflate(layoutInflater)
