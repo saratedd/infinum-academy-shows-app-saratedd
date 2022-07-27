6c182b91336d4e50e89d3b972b580e092e2267bb
saratedd <sara.tedesko91@gmail.com>
2022-07-27 22:12:53 +0200

    create fragment_register

---
+++ app/src/main/java/com/example/shows_saratedd/RegisterFragment.kt
@@ -0,0 +1,9 @@
+package com.example.shows_saratedd
+
+import androidx.fragment.app.Fragment
+
+class RegisterFragment : Fragment() {
+
+//    private var _binding: FragmentLoginBinding? = null
+//    private val binding get() = _binding!!
+}
