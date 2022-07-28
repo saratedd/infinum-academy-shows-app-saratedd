0a9a62b468e67a726a0c3d1e5a69f070f1b08062
saratedd <sara.tedesko91@gmail.com>
2022-07-28 15:49:36 +0200

    add showresponse

--- app/src/main/java/com/example/shows_saratedd/ShowsApiService.kt
+++ app/src/main/java/com/example/shows_saratedd/ShowsApiService.kt
@@ -3,8 +3,10 @@ package com.example.shows_saratedd
 import com.example.shows_saratedd.login.LoginRequest
 import com.example.shows_saratedd.login.LoginResponse
 import com.example.shows_saratedd.register.RegisterRequest
+import com.example.shows_saratedd.shows.ShowsResponse
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface ShowsApiService {
@@ -14,4 +16,7 @@ interface ShowsApiService {
 
     @POST("/users/sign_in")
     fun login(@Body request: LoginRequest) : Call<LoginResponse>
+
+    @GET("/shows")
+    fun getShowsAlpha() : Call<ShowsResponse>
 }
