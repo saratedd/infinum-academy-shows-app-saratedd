6b861b6f529737f28c747da29ac327ca5a7aeded
saratedd <sara.tedesko91@gmail.com>
2022-08-07 19:28:12 +0200

    no commented code

--- app/src/main/java/com/example/shows_saratedd/MainActivity.kt
+++ app/src/main/java/com/example/shows_saratedd/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(R.layout.activity_main)
+
         setContentView(binding.root)
     }
 }
